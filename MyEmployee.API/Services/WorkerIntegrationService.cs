177e169730bb532748045be2cb23f45c69c1cb17
shinus9008 <shinus9008@gmail.com>
2023-12-08 21:28:25 +0500

    Добавил CRUD операций на сервер (без реализации)

--- MyEmployee.API/Services/WorkerIntegrationService.cs
+++ MyEmployee.API/Services/WorkerIntegrationService.cs
@@ -1,9 +1,7 @@
 ﻿using Grpc.Core;
-using MyEmployee.API;
 using System.Collections.Concurrent;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Security.Cryptography.X509Certificates;
 
 namespace MyEmployee.API.Services
 {
@@ -20,47 +18,37 @@ namespace MyEmployee.API.Services
         //    // Пока клиент не завершит передачу подключение сохраняется
         //    await foreach (var message in requestStream.ReadAllAsync()) 
         //    { 
-                
+
         //    }
         //}
 
-        public override async Task GetWorkerStream(
-            EmptyMessage request, 
-            IServerStreamWriter<WorkerAction> responseStream, ServerCallContext context)
+        public override Task GetEmployeeList(EmployeeListRequest request, IServerStreamWriter<EmployeeListReply> responseStream, ServerCallContext context)
         {
-            // TODO: Аутентификация
-
-            // Потоковая передача на стороне сервера
-
-            try
+            return base.GetEmployeeList(request, responseStream, context);
+        }
+        public override Task GetEmployeeStream(EmptyMessage request, IServerStreamWriter<EmployeeReply> responseStream, ServerCallContext context)
+        {
+            return base.GetEmployeeStream(request, responseStream, context);
+        }
+        public override Task<EmployeeReply> CreateEmployee(CreateEmployeeRequest request, ServerCallContext context)
+        {
+            return base.CreateEmployee(request, context);
+        }
+        public override Task<EmployeeReply> UpdateEmployee(UpdateEmployeeRequest request, ServerCallContext context)
+        {
+            return base.UpdateEmployee(request, context);
+        }
+        public override Task<EmployeeReply> DeleteEmployee(UpdateEmployeeRequest request, ServerCallContext context)
+        {
+            return base.DeleteEmployee(request, context);
+        }
+        public override async Task GetWorkerStream(EmptyMessage request, IServerStreamWriter<WorkerAction> responseStream, ServerCallContext context)
+        {
+            // Возвращает всех 
+            foreach (WorkerMessage message in EmployeeWorker.items.Values.ToArray())
             {
-                // Возвращает всех 
-                foreach (WorkerMessage message in EmployeeWorker.items.Values.ToArray()) 
-                {
-                    await responseStream.WriteAsync(new WorkerAction { ActionType = Action.Default, Worker = message });
-                }
-
+                await responseStream.WriteAsync(new WorkerAction { ActionType = Action.Default, Worker = message });
             }
-            catch (Exception ex)
-            {
-                //TODO: Логер!
-            }
-
-            // Потоковая передача завершается
-
-
-
-            // Сохраняем его как подписчика
-
-            // Слушаем сообщения клиента
-
-            // Если сообщения закончились
-
-            // Отписываемся 
-           
-
-
-
         }
     }
 
